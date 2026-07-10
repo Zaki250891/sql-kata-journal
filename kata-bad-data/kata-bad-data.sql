aa6a5b7713e94eb30c2b358d1ca6dffda7f23795
zak <dunozak@gmail.com>
2026-07-10 17:19:00 -0600

    wrong-address-customers

--- kata-bad-data/kata-bad-data.sql
+++ kata-bad-data/kata-bad-data.sql
@@ -15,8 +15,24 @@ no_address_customers AS (
     left join addresses a
     on fd.customerid = a.customerid
     where a.addressid is null
+),
+
+wrong_address_customers AS (
+    select 
+        fd.customerid,
+        fd.total_price,
+        s.addressid
+    from filtered_data fd
+    left join sales s
+    on fd.customerid = s.customerid
+    left join addresses a
+    on s.addressid = a.addressid
+    and s.customerid = a.customerid
+    where a.addressid is null
+    
 )
 
 
+
 select *
-from no_address_customers
+from wrong_address_customers
