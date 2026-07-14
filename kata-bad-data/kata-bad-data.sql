0a9cb313b323a6149113f22cb0a548b6e1db04ae
zak <dunozak@gmail.com>
2026-07-14 09:03:22 -0600

    operator

--- kata-bad-data/kata-bad-data.sql
+++ kata-bad-data/kata-bad-data.sql
@@ -1,7 +1,7 @@
 with filtered_data as (
     select 
         customerid,
-        SUM(price) as total_price
+        SUM(price) as total_bought
     from sales
     group by customerid
     having SUM(price) >= 199
@@ -9,7 +9,7 @@ with filtered_data as (
 no_address_customers AS (
     select 
         fd.customerid,
-        fd.total_price,
+        fd.total_bought,
         'no address' as issue
     from filtered_data fd
     left join addresses a
@@ -19,7 +19,7 @@ no_address_customers AS (
 wrong_address_customers AS (
     select 
         fd.customerid,
-        fd.total_price,
+        fd.total_bought,
         'wrong_address' as issue
     from filtered_data fd
     where exists (
@@ -42,24 +42,36 @@ wrong_address_customers AS (
 customers_to_contact as (
     select 
         nac.customerid,
-        nac.total_price,
+        nac.total_bought,
         nac.issue
     from no_address_customers nac
     union all
     select 
         wac.customerid,
-        wac.total_price,
+        wac.total_bought,
         wac.issue
     from wrong_address_customers wac
 ),
-
 ordered_customers as (
     select 
         customerid,
-        total_price,
-        issue
+        total_bought,
+        issue,
+        row_number() over (order by total_bought desc) as rn
     from customers_to_contact
-    order by total_price desc
+),
+assigned_reps as (
+    select 
+        oc.customerid,
+        oc.total_bought,
+        oc.issue,
+        oc.rn,
+        case 
+            when rn % 3 = 1 then 'Rep A'
+            when rn % 3 = 2 then 'Rep B'
+            else 'Rep C'
+        end as assigned_rep
+    from ordered_customers oc
 )
 
 select *
